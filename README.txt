b6c407c776b7deed2f52403468fa69022dcd1d96
melon-jelly <chelsea110501@gmail.com>
2021-06-16 17:32:54 +0900

    Updated
    - This is a small project for studying tkinter.
    - Aded some functions!

---
+++ README.txt
@@ -0,0 +1,4 @@
+README
+This is a small project for studying tkinter!
+Purpose of this project is building a dictionary with the energy engineering terms I found at Korean DataPortal(https://www.data.go.kr/).
+Anyone who's studying tkinter, please feel free to use the code and source:)
--- energy_engin_dictionary.py
+++ energy_engin_dictionary.py
@@ -12,18 +12,27 @@ dat = pd.read_excel("./energy_engin_dic.xlsx")
 def click():
     print("버튼이 클릭되었습니다.")
     word = entry.get()
-    output.delete(0.0, END)
+    output1.delete(0.0, END)
+    output2.delete(0.0, END)
+    output3.delete(0.0, END)
     print("{}을 검색했습니다".format(word))
-    try:
-        def_word = dat.loc[dat['한글명'] == word, '내용'].values[0]
-    except:
-        def_word = "단어의 뜻을 찾을 수 없음."
+    # try:
+    def_word1 = dat.loc[dat['한글명'] == word, '한문명'].values[0]
+    def_word2 = dat.loc[dat['한글명'] == word, '영문명'].values[0]
+    def_word3 = dat.loc[dat['한글명'] == word, '내용'].values[0]
+    # except:
+    #     def_word1 = "단어의 뜻을 찾을 수 없음."
+    #     def_word2 = "단어의 뜻을 찾을 수 없음."
+    #     def_word3 = "단어의 뜻을 찾을 수 없음."
 
-    output.insert(END, def_word)
+    output1.insert(END, def_word1)
+    output2.insert(END, def_word2)
+    output3.insert(END, def_word3)
 
 window = Tk()
 window.title("발전용어사전")
 
+#입력창
 label = Label(window, text="원하는 단어 입력 후 엔터를 눌러주세요")
 label.grid(row=0, column=0, sticky=W)
 
@@ -33,11 +42,26 @@ entry.grid(row=1,column=0,sticky=W)
 btn = Button(window, text = "제출", width=5, command =click)
 btn.grid(row=2,column=0,sticky=W)
 
-label2 = Label(window, text ="\n의미:")
-label2.grid(row=3, column=0, sticky=W)
+#한문
+label1 = Label(window, text ="\n한문:")
+label1.grid(row=3, column=0, sticky=W)
 
-output = Text(window, width=50, height=6, wrap=CHAR, background="light green")
-output.grid(row=4, column=0, columnspan=2, sticky=W)
+output1 = Text(window, width=50, height=1, wrap=CHAR, background="light green")
+output1.grid(row=4, column=0, columnspan=2, sticky=W)
+#영문
+label2 = Label(window, text ="\n영문:")
+label2.grid(row=5, column=0, sticky=W)
+
+output2 = Text(window, width=50, height=1, wrap=CHAR, background="light green")
+output2.grid(row=6, column=0, columnspan=2, sticky=W)
+
+
+#의미창
+label3 = Label(window, text ="\n의미:")
+label3.grid(row=7, column=0, sticky=W)
+
+output3 = Text(window, width=50, height=6, wrap=CHAR, background="light green")
+output3.grid(row=8, column=0, columnspan=2, sticky=W)
 
 # 메인 반복문 실행
 window.mainloop()
